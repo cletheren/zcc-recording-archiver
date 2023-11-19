2b022afbefec515ef3e5f501df98a0bf66dfa154
cletheren <craigletheren@gmail.com>
2023-11-19 20:03:36 +0000

    Added docstrings

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-Python script to download recordings from Zoom Contact Center
+# zcc-recording-archiver
+
+## Python script to download historical recordings from Zoom Contact Center
+
+This script will query the ZCC API for historic recordings based on a date/time window specified in the main.py file (refer to **START_DATE** and **END_DATE** variables). Each recording found will create an instance of the Recording class, and a list of Recording objects will be returned.
+
+Recording objects have a **download** method which can be used to download the recording to the path specified in the **RECORDING_PATH** variable. The list of recording objects is interated over, and the `download` method is called on each. The `download` method will perform a GET request to the download URL and store the recording locally within the specified path (refer to the **RECORDING_PATH** variable)
+
+To run this script you must install the modules shown in the **requirements.txt** file by running the following command.
+
+`pip install -r requirements.txt`
+
+Next, open the `main.py` file and modify the following variables:
+
+**START_DATE**: The start date/time of the date range that you'd like to query
+**END_DATE**: The end date/time of the date range that you'd like to query
+**RECORDING_PATH**: The path to the location where you would like to store the downloaded recordings
+
+You must login to <https://marketplace.zoom.us> and create a new Server-to-Server OAuth app with the `contact_center_recording:read:admin` scope enabled. Once this is created you must populate the **ACCOUNT_ID**, **CLIENT_ID** and **CLIENT_SECRET** environment variables using the values from your Server-to-Server app. Rename the `.env_sample` file to `.env` and populate these values here. Note, the use of quotation marks is NOT required in the .env file.
+
+Further information about Server-to-Server OAuth can be found here:
+
+<https://marketplace.zoom.us/docs/guides/build/server-to-server-oauth-app/>
+
+Information about the ZCC API endpoint used to provide historical recordings can be found here:
+
+<https://developers.zoom.us/docs/api/rest/reference/contact-center/methods/#operation/listRecordings>
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from zoom import Client
 # Configure logging
 logging.basicConfig(format="%(levelname)s:%(asctime)s %(message)s", datefmt="%d/%m/%Y %H:%M:%S", level=logging.INFO)
 
-# Load the required environment variables from .env
+# Load the environment variables from .env file. These should be populated with information from the Zoom S2S marketplace app.
 load_dotenv()
 ZOOM_ACCOUNT_ID = os.environ.get("ZOOM_ACCOUNT_ID")
 ZOOM_CLIENT_ID = os.environ.get("ZOOM_CLIENT_ID")
@@ -20,13 +20,36 @@ ZOOM_CLIENT_SECRET = os.environ.get("ZOOM_CLIENT_SECRET")
 # Set the path where you would like to store the recordings
 RECORDING_PATH = Path("/Users/cletheren/Recordings")
 
-# Set the timeframe for the download
+# Set the timeframe for the download. Use ISO 8601 formatted strings.
 START_DATE = "2023-10-01T00:00:00"
 END_DATE = "2023-10-31T23:59:59"
 
     
 class Recording:
-    """Object to represent a recorded asset"""
+    """Object to represent a recorded asset
+
+    The __init__ method may be documented in either the class level
+    docstring, or as a docstring on the __init__ method itself.
+
+    Either form is acceptable, but the two should not be mixed. Choose one
+    convention to document the __init__ method and be consistent with it.
+
+    Args:
+        start_time (str): The time and date the recording started, in ISO 8601 format.
+        engagement_id (str): The unique identifier for the engagement from the Zoom API.
+        channel_type (str): The channel type, either voice, video, chat or sms.
+        recording_id (str): The unique identifier for the recording from the Zoom API.
+        download_url (str): The URL provided by the Zoom API that is used to download the recording.
+
+    Attributes:
+        start_time (str): The time and date the recording started, in ISO 8601 format.
+        engagement_id (str): The unique identifier for the engagement from the Zoom API.
+        channel_type (str): The channel type, either voice, video, chat or sms.
+        recording_id (str): The unique identifier for the recording from the Zoom API.
+        download_url (str): The URL provided by the Zoom API that is used to download the recording.
+        filename (str): The filename generated based on start_time, engagement_id, recording_id and channel_type.
+
+    """
 
     def __init__(
             self,
@@ -41,8 +64,20 @@ class Recording:
         self.channel_type = channel_type
         self.recording_id = recording_id
         self.download_url = download_url
+        self.filename = f"{start_time}_{self.engagement_id}_{self.recording_id}.{"mp3" if self.channel_type == "voice" else "mp4"}"
 
-    def download(self, client: Client, path: Path) -> None:
+    def download(self, client: Client, path: Path) -> bool:
+        """Method to download the recorded asset represented by the object. This method will accept a client connection
+        object containing a valid bearer token for accessing the Zoom API, and a Path object containing the path where
+        the recording should be saved. 
+
+        Args:
+            client (zoom.Client): The Zoom client connection object that contains the bearer token and base URL.
+            path (pathlib.Path): A path object, where the recording should be stored locally. 
+        
+        Returns:
+            True if successful. Any failed attempt to download will exit with sys.exit(1)
+        """
         headers = {
             "Authorization": f"Bearer {client.token}"
         }
@@ -52,10 +87,8 @@ class Recording:
                 path.mkdir(parents=True)
         except PermissionError as err:
             logging.info(f"{err}, please check your RECORDING_PATH and try again")
-            quit()
-        extension = "mp3" if self.channel_type == "voice" else "mp4"
-        filename = f"{start_time}_{self.engagement_id}_{self.recording_id}.{extension}"
-        filename = Path(path, filename)
+            exit(1)
+        filename = Path(path, self.filename)
         with requests.Session() as req:
             if client.token_has_expired:
                     logging.debug("Bearer token has expired, generating a new one...")
@@ -71,12 +104,22 @@ class Recording:
                         f.write(chunk)
             except requests.HTTPError as err:
                 logging.warning(err)
-        return None
+        return True
         
     def __repr__(self):
         return f"Recording(start_time={self.start_time!r}, engagement_id={self.engagement_id!r}, recording_id={self.recording_id!r}, channel_type={self.channel_type!r}, download_url={self.download_url!r})"
 
-def get_recording_list(client: Client) -> list[Recording]:
+def get_recording_list(client: Client, start_range: str, end_range: str) -> list[Recording]:
+    """Method to query the Zoom API to obtain a list of recordings based on the start and end date range provided.
+
+    Args:
+        client (zoom.Client): The Zoom client connection object that contains the bearer token and base URL.
+        start_range (str): The starting date & time in ISO 8601 format, example '2023-09-01T00:00:00'.
+        end_range (str): The ending date & time in ISO 8601 format, example '2023-09-30T23:59:59'.
+
+    Returns:
+        A list of Recording objects. If no recordings are returned by the API then an empty list is returned.
+    """
     logging.info("Getting list of recordings...")
     recording_list = []
     endpoint = f"{client.base_url}/contact_center/recordings"
@@ -84,8 +127,8 @@ def get_recording_list(client: Client) -> list[Recording]:
         "Authorization": f"Bearer {client.token}",
     }
     params = {
-        "from": START_DATE,
-        "to": END_DATE,
+        "from": start_range,
+        "to": end_range,
         "channel_type": "voice",
         "next_page_token": ""
     }
@@ -125,7 +168,7 @@ def main() -> None:
     client = Client(ZOOM_CLIENT_ID, ZOOM_CLIENT_SECRET, ZOOM_ACCOUNT_ID)
     client.get_token()
     logging.info(f"Recording path is {RECORDING_PATH}")
-    recording_list = get_recording_list(client)
+    recording_list = get_recording_list(client, START_DATE, END_DATE)
     if recording_list:
         for recording in recording_list:
             recording.download(client, RECORDING_PATH)
